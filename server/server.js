2a626337c0f1da7f20e8bace7ffeb5835dbdfe84
Harjot Singh <harry30363@gmail.com>
2024-10-21 11:15:51 +0530

    setup

---
+++ server/server.js
@@ -0,0 +1,12 @@
+//FRAMEWORK CONFIGURATION
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("cars");
+const cors = require("cors");
+
+connectDb();
+const app = express();
+const port= process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
